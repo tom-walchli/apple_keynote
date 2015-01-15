d084847a1c682f5f0158d5201c174c9b61e2fbeb
Tom Walchli <tom@wit-americas.com>
2015-01-15 17:06:26 -0500

    now with classes :-)

--- keynote.rb
+++ keynote.rb
@@ -10,93 +10,88 @@
 ####################################################################
 
 require 'terminfo'
- 
-def measure(dimension,length=1)
-  @dimension 	= dimension 	# height or width [ 0 | 1 ]
-  @length	= length
-#  puts @length
-#  puts @dimension
-#  puts "ss: #{$screensize[@dimension]}"
-  delta = (($screensize[@dimension] - @length) / 2).floor
-#  puts "delta: #{delta}"
-  return delta
-end
-
-def waitForNext
-  nextChar = gets.chomp
-  if nextChar == 'z'
-    toggle(-1)
-  elsif nextChar == 'a'
-    $autoMode = true
-    toggle()
-  else 
-    toggle()
-  end
-end
-
-def runAuto
-  sleep($sleepDelay)
-  if $currIndex == $slides.length - 1
-    return
-  end
-  toggle
-  runAuto
-end
-
-def toggle(direction=1)
-  $currIndex += direction
-  if $currIndex < 0 
-    $currIndex = 0
-  elsif $currIndex > $slides.length - 1
-    $currIndex = $slides.length - 1
-  end
-  selSlide
-end  
-
-def selSlide
-  display($slides[$currIndex])
-end
-
-def display_old(slide)
-  t = TermInfo.new(ENV["TERM"], STDOUT)
-  $screensize = t.screen_size
-  #system "clear" #
-  system "cls"
-  dY = measure(0)
-  dX = measure(1,slide.length)
-  t.control ("cuf",dX)
-  t.control ("cud",dY)
-  puts slide
-  print "\n"*(dY-1)
-  $autoMode ? runAuto : waitForNext()
-end
-
-def display(slide)
-  t = TermInfo.new(ENV["TERM"], STDOUT)
-  $screensize = t.screen_size
-  system "clear" 
-#  system "cls"
-  dY = measure(0,slide.split("\n").length)
-  prepare(slide)
-  print "\n"*dY
-  print  prepare(slide)
-  print "\n"*(dY-1)
-  $autoMode ? runAuto() : waitForNext()
-end
-
-def prepare(slide)
-  linesArr = slide.split("\n")
-  maxLength = (linesArr.max {|a,b| a.length <=> b.length}).length
-  dX = measure(1,maxLength)
-  return " "*dX + linesArr.join("\n" + " "*dX)
-  
-end
 
 $sleepDelay = 1.5
 $currIndex = 0
 $slides = IO.read('slides_multi_line.txt').split("\n----\n")
 $autoMode = false
-selSlide
+$screenSize = []
+
+class PrepareSlide
+	def self.measure(dimension,length=1)
+#		puts "Dim: #{dimension} Length: #{length} "
+#		puts $screenSize
+		delta = (($screenSize[dimension] - length) / 2).floor
+		return delta
+	end
+
+	def self.prepare(slide)
+		linesArr = slide.split("\n")
+		maxLength = (linesArr.max {|a,b| a.length <=> b.length}).length
+		dX = self.measure(1,maxLength)
+		return " "*dX + linesArr.join("\n" + " "*dX)
+	end
+end
+
+class RunAuto
+	def self.runAuto
+		sleep($sleepDelay)
+		if $currIndex == $slides.length - 1
+			return
+		end
+		SelectSlide.toggle
+		self.runAuto
+	end
+end
+
+class SelectSlide
+
+	def self.selSlide
+		Display.new($slides[$currIndex])
+	end
+
+	def self.toggle(direction=1)
+		$currIndex += direction
+		if $currIndex < 0 
+			$currIndex = 0
+		elsif $currIndex > $slides.length - 1
+			$currIndex = $slides.length - 1
+		end
+		self.selSlide
+	end  
+
+	def self.waitForNext
+		nextChar = gets.chomp
+		if nextChar == 'z'
+			toggle(-1)
+		elsif nextChar == 'a'
+			$autoMode = true
+			toggle()
+		else 
+			toggle()
+		end
+	end
+end
+
+class Display
+	def initialize(slide)
+		display(slide)
+	end
+
+	def display(slide)
+		t = TermInfo.new(ENV["TERM"], STDOUT)
+		$screenSize = t.screen_size
+		system "clear" 
+		dY = PrepareSlide.measure(0,slide.split("\n").length)
+#		PrepareSlide.prepare(slide)
+		print "\n"*dY
+		print PrepareSlide.prepare(slide)
+		print "\n"*(dY-1)
+		$autoMode ? RunAuto.runAuto() : SelectSlide.waitForNext()
+	end
+end
+
+SelectSlide.selSlide
 
 
 
